a792ab0cba521eab1a9a725c2fc8c7808efbb8d6
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-09-22 18:11:24 +0000

    done

--- area_per_rect_input.py
+++ area_per_rect_input.py
@@ -8,12 +8,14 @@
 length = int(input("Enter length of the rectangle (cm): "))
 width = int(input("Enter width of the rectangle (cm): "))
 
+
 # Function to calculate the area and perimeter of a rectangle
 def main():
     print("The Perimeter and Area of a Rectangle is:")
     print()
     print("The area is: {}cm^2".format(length * width))
     print("The perimeter is: {}cm".format(2 * (length + width)))
+
 
 if __name__ == "__main__":
     main()
